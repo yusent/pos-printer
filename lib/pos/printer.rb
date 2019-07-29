2d9def4bf19f91c72a55aae79e6a08212564cd17
Yusent Chig <yusent@protonmail.com>
2019-07-29 16:57:06 -0700

    Add print method

--- lib/pos/printer.rb
+++ lib/pos/printer.rb
@@ -1,7 +1,24 @@
+require 'open3'
+
 module POS
   class Printer
-    def self.hi
-      puts "Hello world!"
+    def initialize(name)
+      @name = name
+      @commands = ''
+    end
+
+    def commands
+      @commands.gsub(/'/, "'\"'\"'")
+    end
+
+    def print
+      send_to_printer
+    end
+
+    private
+
+    def send_to_printer
+      Open3.capture3('lp', '-d', @name, '-o', 'raw', stdin_data: commands)
     end
   end
 end
